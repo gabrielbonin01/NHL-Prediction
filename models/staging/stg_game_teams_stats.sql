9e27f66718eb58ed9104e37bc9bc67ac96c532bd
gabrielbonin01 <78220230+gabrielbonin01@users.noreply.github.com>
2022-03-04 12:25:43 -0500

    Create stg_game_teams_stats.sql

---
+++ models/staging/stg_game_teams_stats.sql
@@ -0,0 +1,24 @@
+with source_datalake as (
+    select 
+        GAME_ID,
+        HOA,
+        WON,
+        SETTLED_IN,
+        HEAD_COACH,
+        GOALS,
+        SHOTS,
+        HITS,
+        PIM,
+        POWERPLAY_OPPORTUNITIES,
+        POWERPLAY_GOALS,
+        FACEOFF_WIN_PERCENTAGE,
+        GIVEAWAYS,
+        TAKEAWAYS,
+        BLOCKED,
+        STARTRINKSIDE,
+        current_timestamp() as ingestion_timestamp
+    from {{ source('DATALAKE', 'GAME_TEAMS_STATS') }}       
+)
+
+select *
+from source_datalake
