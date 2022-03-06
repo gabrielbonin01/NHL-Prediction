35542c3c25402f6e3652373211b920f41b6eeaca
gabrielbonin01 <78220230+gabrielbonin01@users.noreply.github.com>
2022-03-06 11:33:48 -0500

    Create dim_game.sql

---
+++ models/warehouse_dim_fact/dim_game.sql
@@ -0,0 +1,25 @@
+with source as(
+select *,g.DATE_TIME_GMT as DATE_TIME
+from "NHL_PREDICTION"."STAGING_LAYER"."STG_GAME" as g
+inner join "NHL_PREDICTION"."STAGING_LAYER"."STG_DATE" as d
+where g.DATE_TIME_GMT= d.DATE_TIME_GMT
+)
+select 
+        GAME_ID,
+        SEASON,
+        GAME_TYPE,
+        DATE_TIME,
+        DATE_ID,
+        AWAY_TEAM_ID,
+        HOME_TEAM_ID,
+        AWAY_GOALS,
+        HOME_GOALS,
+        HOME_RINK_START,
+        VENUE,
+        VENUE_TIME_ZONE_ID,
+        VENUE_TIME_ZONE_OFFSET,
+        WINNER,
+        REG_OT,
+        current_timestamp() as ingestion_timestamp
+
+from source
