16393f3b4e02d22aaa0b8c2056ca1d4b0abdd863
gabrielbonin01 <78220230+gabrielbonin01@users.noreply.github.com>
2022-03-07 14:42:07 -0500

    Create stg_game_play_players.sql

---
+++ models/staging/stg_game_play_players.sql
@@ -0,0 +1,12 @@
+with source_datalake as (
+        select 
+        PLAY_ID,
+        GAME_ID,
+        PLAYER_ID,
+        PLAYERTYPE,
+        current_timestamp() as ingestion_timestamp
+    from {{ source('DATALAKE', 'GAME_PLAYS_PLAYERS') }}       
+)
+
+select *
+from source_datalake
