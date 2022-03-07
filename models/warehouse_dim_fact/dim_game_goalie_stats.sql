852e44bf6a0b5fa03b5bc6715e0a6d7ab3dcafb4
gabrielbonin01 <78220230+gabrielbonin01@users.noreply.github.com>
2022-03-07 17:08:59 -0500

    Create dim_game_goalie_stats.sql

---
+++ models/warehouse_dim_fact/dim_game_goalie_stats.sql
@@ -0,0 +1,33 @@
+with staging_warehouse as (
+        select 
+        GAME_ID,
+        PLAYER_ID,
+        TEAM_ID,
+        TIME_ON_ICE as TIME_ON_ICE_GOALIE,
+        ASSISTS as ASSISTS_GOALIE,
+        GOALS as GOALS_GOALIE,
+        PIM as PIM_GOALIE,
+        SHOTS as SHOTS_GOALIE,
+        SAVES,
+        POWER_PLAY_SAVES,
+        SHORT_HANDED_SAVES,
+        EVEN_SAVES,
+        SHORT_HANDED_SHOTS_AGAINST,
+        EVEN_SHOTS_AGAINST,
+        POWERPLAY_SHOTS_AGAINST,
+        SAVE_PERCENTAGE,
+        POWERPLAY_SAVE_PERCENTAGE,
+        EVEN_STRENGTH_SAVE_PERCENTAGE,
+        current_timestamp() as ingestion_timestamp,
+        case
+        WHEN DECISION is NULL THEN 'OUT OF GAME'
+        WHEN DECISION = 'W' THEN 'WIN GAME'
+        WHEN DECISION = 'L' THEN 'LOST GAME'
+        END as DECISION
+    from {{ ref('stg_game') }}
+)
+
+select *
+from staging_warehouse
+
+
