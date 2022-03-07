6d072d554c89c925c43afe00bc4f11cc2ed0e230
gabrielbonin01 <78220230+gabrielbonin01@users.noreply.github.com>
2022-03-07 17:08:19 -0500

    Update stg_game_officials.sql

--- models/staging/stg_game_officials.sql
+++ models/staging/stg_game_officials.sql
@@ -1,4 +1,4 @@
-with source_datalake as (
+with source_staging as (
         select 
         game_id,
         official_name,
@@ -8,4 +8,4 @@ with source_datalake as (
 )
 
 select *
-from source_datalake
+from source_staging
