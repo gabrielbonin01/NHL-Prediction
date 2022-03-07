091df99f2bdef9c73a214816a1f09d1338625f39
gabrielbonin01 <78220230+gabrielbonin01@users.noreply.github.com>
2022-03-07 18:13:55 -0500

    Update dim_date.sql

--- models/warehouse_dim_fact/dim_date.sql
+++ models/warehouse_dim_fact/dim_date.sql
@@ -1,6 +1,15 @@
 with staging_warehouse as(
-select *
-from from {{ ref('stg_date') }}
+    select
+    DATE_ID,
+    DATE_TIME_GMT,
+    YEAR,
+    MONTH,
+    DAY,
+    WEEK,
+    QUARTER,
+    HOUR,
+    MINUTE ,
+from {{ ref('stg_date') }}
 )
 
 select * from staging_warehouse
