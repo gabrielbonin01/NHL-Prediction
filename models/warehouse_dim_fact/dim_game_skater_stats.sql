2265d1eaab649390027a38c77551d9797267d7d5
gabrielbonin01 <78220230+gabrielbonin01@users.noreply.github.com>
2022-03-07 18:14:26 -0500

    Create dim_game_skater_stats.sql

---
+++ models/warehouse_dim_fact/dim_game_skater_stats.sql
@@ -0,0 +1,30 @@
+with staging_warehouse as (
+        select 
+        GAME_ID,
+        PLAYER_ID,
+        TEAM_ID,
+        TIME_ON_ICE as TIME_ON_ICE_SKATER,
+        ASSISTS as ASSISTS_SKATER,
+        GOALS as GOALS_SKATER,
+        SHOTS as SHOTS_SKATER,
+        HITS as HITS_SKATER,
+        POWER_PLAY_GOALS as POWER_PLAY_GOALS_SKATER,
+        POWER_PLAY_ASSISTS as POWER_PLAY_ASSISTS_SKATER,
+        PENALITY_MINUTES,
+        FACEOFF_WINS,
+        FACEOFF_TAKEN,
+        TAKEAWAYS as TAKEAWAYS_SKATER,
+        GIVEAWAYS as GIVEAWAYS_SKATER,
+        SHORT_HANDED_GOALS,
+        SHORT_HANDED_ASSISTS,
+        BLOCKED,
+        PLUS_MINUS,
+        EVEN_TIME_ON_ICE,
+        SHORT_HANDED_TIME_ON_ICE,
+        POWERPLAY_TIME_ON_ICE,
+        current_timestamp() as ingestion_timestamp
+    from {{ ref('stg_game_skater_stats') }}       
+)
+
+select *
+from staging_warehouse
