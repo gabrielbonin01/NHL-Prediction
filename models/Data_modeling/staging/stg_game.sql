f4a102f7c773dbd8e3ae1a7fdbce23037397e599
gabrielbonin01 <78220230+gabrielbonin01@users.noreply.github.com>
2022-03-07 23:36:49 -0500

    Create stg_game.sql

---
+++ models/Data_modeling/staging/stg_game.sql
@@ -0,0 +1,25 @@
+with source_staging as (
+        select 
+        GAME_ID,
+        SEASON,
+        GAME_TYPE,
+        DATE_TIME_GMT,
+        AWAY_TEAM_ID,
+        HOME_TEAM_ID,
+        AWAY_GOALS,
+        HOME_GOALS,
+        HOME_RINK_START,
+        VENUE,
+        VENUE_TIME_ZONE_ID,
+        VENUE_TIME_ZONE_OFFSET, 
+        left(OUTCOME, CHARINDEX(' ', OUTCOME) - 1) as WINNER, 
+        right(OUTCOME, CHARINDEX(' ', OUTCOME) - 2) as REG_OT,
+        current_timestamp() as ingestion_timestamp
+    from {{ source('DATALAKE', 'GAME') }}       
+)
+
+SELECT ROW_NUMBER() OVER (ORDER BY DATE_TIME_GMT) AS DATE_ID, *
+
+FROM source_staging
+
+
